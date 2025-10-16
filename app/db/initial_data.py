35c3bd7a04beaace7309b7dbf1566ba8ef18ea15
Parallax73 <narakagaara@gmail.com>
2025-10-16 14:40:50 -0300

    Refactored initial data descriptions

--- app/db/initial_data.py
+++ app/db/initial_data.py
@@ -22,29 +22,39 @@ async def create_initial_data(db: AsyncSession):
     try:
         # 2. Crie os objetos de Sinais (Signs)
         sign_bom_dia = Sign(
-            name="Bom dia",
-            desc="",
-            videoUrl="",
-            pontos=10
-        )
+        name="Bom dia",
+        desc="Este é um sinal composto por duas partes: <b>BOM</b> e <b>DIA</b>.<br><br>" \
+            "<b>BOM:</b> Com a palma virada para você, junte as pontas dos dedos em frente à boca. Mova a mão para frente, abrindo e espalhando os dedos.<br>" \
+            "<b>DIA:</b> Em seguida, com a mão em 'D' (indicador para cima, outros dedos em círculo com o polegar), faça um arco da direita para a esquerda, simbolizando o sol.",
+        videoUrl="bom_dia",
+        pontos=10
+            )
+
         sign_obrigado = Sign(
             name="Obrigado",
-            desc="",
-            videoUrl="",
+            desc="Este sinal é realizado com as duas mãos.<br><br>" \
+                "Posicione a mão direita tocando o queixo e a mão esquerda tocando a testa. Depois, mova <b>ambas as mãos</b> para a frente, em um gesto de oferecimento.",
+            videoUrl="obrigado",
             pontos=10
         )
+
         sign_tudo_bem = Sign(
             name="Tudo bem?",
-            desc="",
-            videoUrl="",
+            desc="Este sinal é feito em duas partes, unindo <b>BOM</b> e o gesto de <b>JOIA</b>.<br><br>" \
+                "<b>BOM:</b> Junte as pontas dos dedos em frente à boca com a palma para você, e então abra a mão para frente.<br>" \
+                "<b>JOIA:</b> Logo em seguida, mude a configuração da mão para o sinal de 'joia' (polegar para cima).",
+            videoUrl="tudo_bem",
             pontos=15
         )
-        sign_qual_seu_nome= Sign(
-            name="Tudo bem?",
-            desc="",
-            videoUrl="",
+
+        sign_qual_seu_nome = Sign(
+            name="Qual seu nome?",
+            desc="O sinal para 'Qual seu nome?' é feito da seguinte forma:<br><br>" \
+                "Com a mão direita, levante os dedos <b>indicador e médio</b>. Faça um movimento de meia-lua com a mão, da esquerda para a direita.",
+            videoUrl="qual_seu_nome",
             pontos=15
         )
+
         
 
         db.add_all([sign_bom_dia, sign_obrigado, sign_tudo_bem, sign_qual_seu_nome])
